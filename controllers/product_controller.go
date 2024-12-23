295bf7df480aa9a1279250ac89f709e214278086
Roman Keanu Mufid <romankeanumufid@gmail.com>
2024-12-23 23:25:15 +0700

    update: create model and handler

---
+++ controllers/product_controller.go
@@ -0,0 +1,10 @@
+package controllers
+
+import "github.com/romanmufid16/golang-rest-api/models"
+
+var product = []models.Product{
+	{ID: 1, Name: "Laptop", Price: 1000, Quantity: 10},
+	{ID: 2, Name: "Mouse", Price: 10, Quantity: 50},
+}
+
+
---
+++ models/product.go
@@ -0,0 +1,8 @@
+package models
+
+type Product struct {
+	ID int `json:"id"`
+	Name string `json:"id"`
+	Price float64 `json:"price"`
+	Quantity int `json:"quantity"`
+}
